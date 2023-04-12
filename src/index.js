d1e156f1a55ca322b3f13d9a75db41807e30203b
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-04-12 11:49:42 +0600

    Server Side projects setup completed with connecting database

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// imports
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import path, { join } from "path";
+import connectDB from "./configs/databaseConfigs.js";
+
+// Application
+const app = express();
+dotenv.config();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(join(__dirname, "public")));
+app.use(express.static("uploads"));
+app.use(morgan("dev"));
+
+// Database Connection
+connectDB();
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("HellWet Server Running...");
+});
+
+// listen to port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
