2a94597e3ea8b41437e29716b9e339817c14e26d
MD MONIRUZZAMAN <git config --global user.email>
2023-04-17 15:46:59 +0600

    file serve

--- src/controllers/filesControllers.js
+++ src/controllers/filesControllers.js
@@ -1,11 +1,39 @@
-import path from "path";
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// import path from "path";
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// // serve products images
+// const complainFiles = async (req, res) => {
+//   try {
+//     const imageName = req.params.fileName;
+//     const filePath = path.join(
+//       __dirname,
+//       "uploads",
+//       "files",
+//       "complain_box",
+//       imageName
+//     );
+//     console.log(filePath);
+//     return res.sendFile(filePath);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(400).send({ message: "Invalid Requests!" });
+//   }
+// };
+
+// export { complainFiles };
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { createReadStream } from "fs";
+import { createServer } from "http";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // serve products images
 const complainFiles = async (req, res) => {
   try {
     const imageName = req.params.fileName;
-    const filePath = path.join(
+    const filePath = join(
       __dirname,
       "uploads",
       "files",
@@ -13,10 +41,12 @@ const complainFiles = async (req, res) => {
       imageName
     );
     console.log(filePath);
-    return res.sendFile(filePath);
+    const stream = createReadStream(filePath);
+    stream.pipe(res);
   } catch (error) {
-    console.log(error);
-    return res.status(400).send({ message: "Invalid Requests!" });
+    console.error(error);
+    res.statusCode = 500;
+    res.end("Internal Server Error");
   }
 };
 
