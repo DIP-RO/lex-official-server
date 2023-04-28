27dc30a650931dee2ca543436e71c93483ddb726
MD MONIRUZZAMAN <git config --global user.email>
2023-04-28 12:53:54 +0600

    update

---
+++ src/routes/onlineCourseRoutes.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { UpdateCourseCategory, createCourseCategory, deleteCourseCategory, getAllCourseCategory } from "../controllers/OnlineCourse/courseCategoryControllers";
+import { UpdateOfflinePayment, createOfflinePayment, deleteOfflinePayment, getAllOfflinePayment } from "../controllers/OnlineCourse/OflinePaymentsControllers";
+import { UpdateOnlineCourse, createOnlineCourse, deleteOnlineCourse, getAllOnlineCourse } from "../controllers/OnlineCourse/OnlineCourseControllers";
+import { UpdateSetting, createSetting, deleteSetting, getAllSetting } from "../controllers/OnlineCourse/settingControllers";
+
+const router = Router();
+/*course-category */
+router.post("/course-category", createCourseCategory);
+router.delete("/course-category/:id", deleteCourseCategory);
+router.delete("/course-category/:id", UpdateCourseCategory);
+router.get("/course-category", getAllCourseCategory);
+/* End */
+/*offline-payment */
+router.post("/offline-payment", createOfflinePayment);
+router.delete("/offline-payment/:id", deleteOfflinePayment);
+router.delete("/offline-payment/:id", UpdateOfflinePayment);
+router.get("/offline-payment", getAllOfflinePayment);
+/* End */
+/*online-course */
+router.post("/online-course", createOnlineCourse);
+router.delete("/online-course/:id", deleteOnlineCourse);
+router.delete("/online-course/:id", UpdateOnlineCourse);
+router.get("/online-course", getAllOnlineCourse);
+/* End */
+/*online-course-setting */
+router.post("/online-course-setting", createSetting);
+router.delete("/online-course-setting/:id", deleteSetting);
+router.delete("/online-course-setting/:id", UpdateSetting);
+router.get("/online-course-setting", getAllSetting);
+/* End */
+export default router;
