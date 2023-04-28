81eaaec38664cdee8cc1c791d154d60e49fc2d49
MD MONIRUZZAMAN <git config --global user.email>
2023-04-28 13:01:48 +0600

    updated

--- src/routes/hostelRoutes.js
+++ src/routes/hostelRoutes.js
@@ -23,19 +23,19 @@ const router = Router();
 /* hostel-rooms */
 router.post("/hostel-rooms", createHostelRoom);
 router.delete("/hostel-rooms/:id", deleteHostelRoom);
-router.delete("/hostel-rooms/:id", UpdateHostelRoom);
+router.patch("/hostel-rooms/:id", UpdateHostelRoom);
 router.get("/hostel-rooms", getAllHostelRoom);
 /* End */
 /* room-type */
 router.post("/room-type", createRoomType);
 router.delete("/room-type/:id", deleteRoomType);
-router.delete("/room-type/:id", UpdateRoomType);
+router.patch("/room-type/:id", UpdateRoomType);
 router.get("/room-type", getAllRoomType);
 /* End */
 /* hostel */
 router.post("/hostel", createHostel);
 router.delete("/hostel/:id", deleteHostel);
-router.delete("/hostel/:id", UpdateHostel);
+router.patch("/hostel/:id", UpdateHostel);
 router.get("/hostel", getAllHostel);
 /* End */
 export default router;
--- src/routes/onlineCourseRoutes.js
+++ src/routes/onlineCourseRoutes.js
@@ -8,25 +8,25 @@ const router = Router();
 /*course-category */
 router.post("/course-category", createCourseCategory);
 router.delete("/course-category/:id", deleteCourseCategory);
-router.delete("/course-category/:id", UpdateCourseCategory);
+router.patch("/course-category/:id", UpdateCourseCategory);
 router.get("/course-category", getAllCourseCategory);
 /* End */
 /*offline-payment */
 router.post("/offline-payment", createOfflinePayment);
 router.delete("/offline-payment/:id", deleteOfflinePayment);
-router.delete("/offline-payment/:id", UpdateOfflinePayment);
+router.patch("/offline-payment/:id", UpdateOfflinePayment);
 router.get("/offline-payment", getAllOfflinePayment);
 /* End */
 /*online-course */
 router.post("/online-course", createOnlineCourse);
 router.delete("/online-course/:id", deleteOnlineCourse);
-router.delete("/online-course/:id", UpdateOnlineCourse);
+router.patch("/online-course/:id", UpdateOnlineCourse);
 router.get("/online-course", getAllOnlineCourse);
 /* End */
 /*online-course-setting */
 router.post("/online-course-setting", createSetting);
 router.delete("/online-course-setting/:id", deleteSetting);
-router.delete("/online-course-setting/:id", UpdateSetting);
+router.patch("/online-course-setting/:id", UpdateSetting);
 router.get("/online-course-setting", getAllSetting);
 /* End */
 export default router;
