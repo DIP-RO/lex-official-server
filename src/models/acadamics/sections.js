a9dd20b5de0dab4b50867740f6e77dfb715240d6
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-04-28 10:16:03 +0600

    attendance and acadamics models created and online examination models created

---
+++ src/models/acadamics/sections.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const sectionSchema = new mongoose.Schema({
+  school: {
+    type: String,
+    required: [true, "school id required!"],
+  },
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: [true, "section name required!"],
+  },
+});
+
+const sectionModel = mongoose.model("section", sectionSchema);
+
+export default sectionModel;
---
+++ src/models/acadamics/subjects.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const subjectSchema = new mongoose.Schema({
+  school: {
+    type: String,
+    required: [true, "school id required!"],
+  },
+  created_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  subject: {
+    type: String,
+    required: [true, "subject name required!"],
+  },
+  subjectType: {
+    type: String,
+    required: [true, "subject type required!"],
+  },
+  subjectCode: {
+    type: String,
+    required: [true, "subject code required!"],
+  },
+});
+
+const subjectModel = mongoose.model("subject", subjectSchema);
+
+export default subjectModel;
