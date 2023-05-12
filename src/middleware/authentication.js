d7a963b5dfe3b5ce547db4a3816e4d717bd28690
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2023-05-12 18:37:24 +0600

    Authorization Middleware created

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,37 +1,37 @@
 import jwt from "jsonwebtoken";
 import UserModel from "../models/User.js";
 
-const authenticateUser = async (req, res, next) => {
-  let token;
+const routeRoles = {
+  '/public': ['SUPER_ADMIN', 'ADMIN', 'STUDENT'],
+  '/user': ['STUDENT', 'ADMIN'],
+  '/user/:id': ['STUDENT', 'ADMIN'],
+};
+
+const authorizedUser = async (req, res, next) => {
   const { authorization } = req.headers;
   if (authorization && authorization.startsWith("Bearer")) {
     try {
-      token = authorization.split(" ")[1];
+      const token = authorization.split(" ")[1];
       if (!token) {
-        return res
-          .status(401)
-          .send({ status: "failed", message: "Unauthorized User, No Token" });
+        return res.status(401).send({ status: "failed", message: "access denied!" })
       }
       const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY);
-      req.user = await UserModel.findById(userID).select("-password");
+      const user = await UserModel.findById(userID).select("-password");
+      const userRole = user.role
+      const requiredRoles = routeRoles[req.path];
 
-      return next();
+      if (!requiredRoles || requiredRoles.includes(userRole)) {
+        req.user = user
+        return next();
+      }
+      return res.status(403).send({ status: "failed", message: "access denied!" });        
     } catch (error) {
-      return res
-        .status(403)
-        .send({ status: "failed", message: "Unauthorized User" });
+      return res.status(403).send({ status: "failed", message: "access denied!" });
     }
+  }else{
+    return res.status(401).send({ status: "failed", message: "access denied!" });
   }
 };
 
-const authenticateAdmin = async (req, res, next) => {
-  const requester = req.decoded.email;
-  const user = await userModel.findOne({ email: requester });
-  if (user.role === "ADMIN") {
-    next();
-  } else {
-    res.status(403).send({ message: "forbidden" });
-  }
-};
+export { authorizedUser };
 
-export { authenticateUser, authenticateAdmin };
