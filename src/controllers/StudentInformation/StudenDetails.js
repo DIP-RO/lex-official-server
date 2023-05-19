35f714cbb90767d8a7ca1a722f43a8fa2efb3519
MD MONIRUZZAMAN <git config --global user.email>
2023-05-19 21:26:51 +0600

    student details done

--- src/controllers/StudentInformation/StudenDetails.js
+++ src/controllers/StudentInformation/StudenDetails.js
@@ -1,30 +1,51 @@
 import StudentAdmissionModel from "../../models/StudentInformation/StudentAdmission.js";
 
-const studentDetails = async(req,res)=>{
-    try {
-        const studentClass = req.query.class
-        const section = req.query.section
-        const result = await StudentAdmissionModel.find({class:studentClass,section:section})
-        res.status(200).send(result)        
-    } catch (error) {
-        return res.status(500).send(error.message)
+const studentDetails = async (req, res) => {
+  try {
+    const { name, rollNumber, admissionId, value } = req.query;
+
+    const filters = {};
+
+    if (name) {
+      filters.$or = [
+        { firstName: { $regex: name, $options: "i" } },
+        { lastName: { $regex: name, $options: "i" } },
+      ];
     }
-}
 
-const bulkDelete = async(req,res)=>{
-    try {
-
-        const ids = req.body.ids
-        ids.map(async id=>{
-            await StudentAdmissionModel.findByIdAndRemove(id)
-        })
-
-        return res.status(200).send({message:"Completed!"})
-        
-    } catch (error) {
-        res.status(500).send(error.message)
+    if (rollNumber) {
+      filters.rollNumber = { $regex: rollNumber, $options: "i" };
     }
-}
 
-export { studentDetails ,bulkDelete};
+    if (admissionId) {
+      filters.admissionId = { $regex: admissionId, $options: "i" };
+    }
 
+    if (value) {
+      filters.$or = [
+        { class: { $regex: value, $options: "i" } },
+        { section: { $regex: value, $options: "i" } },
+      ];
+    }
+
+    const result = await StudentAdmissionModel.find(filters);
+    res.status(200).send(result);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const bulkDelete = async (req, res) => {
+  try {
+    const ids = req.body.ids;
+    ids.map(async (id) => {
+      await StudentAdmissionModel.findByIdAndRemove(id);
+    });
+
+    return res.status(200).send({ message: "Completed!" });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export { studentDetails, bulkDelete };
